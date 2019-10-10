132b20d9b124ad01d70aa7e5ecb89357a4923a66
DESKTOP-5TBM399\lx <2513382161@qq.com>
2019-10-10 15:20:09 +0800

    3

---
+++ vue01/src/components/text_zhang/tem_zhang.vue
@@ -0,0 +1,15 @@
+<template>
+    <fieldset>
+      <legend>这是张长庆写的组件</legend>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "tem_zhang"
+    }
+</script>
+
+<style scoped>
+
+</style>
