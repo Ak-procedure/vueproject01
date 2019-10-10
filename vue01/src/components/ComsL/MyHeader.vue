0aad0ff2d7449477a569f2fdcf87e10c7e5b2679
DESKTOP-5TBM399\lx <734906252@qq.com>
2019-10-10 15:43:55 +0800

    1

--- vue01/src/components/ComsL/MyHeader.vue
+++ vue01/src/components/ComsL/MyHeader.vue
@@ -1,24 +1,26 @@
 <template>
-    <div id="MyHeader">
-  <router-link :to="{path:'/zj1'}">组件1</router-link>
-      <router-link :to="{path:'/zj2'}">组件2</router-link>
-    </div>
+  <div id="MyHeader">
+    <router-link :to="{path:'/zj1'}">组件1</router-link>
+    <router-link :to="{path:'/zj2'}">组件2</router-link>
+    <router-link :to="{path:'/zj3'}">组件3</router-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "MyHeader"
-    }
+  export default {
+    name: "MyHeader"
+  }
 </script>
 
 <style scoped>
-#MyHeader{
-  width: 100%;
-  height: 15%;
-  line-height: 100px;
-  background: rgba(0,0,0,0.7);
-}
-  a{
+  #MyHeader {
+    width: 100%;
+    height: 15%;
+    line-height: 100px;
+    background: rgba(0, 0, 0, 0.7);
+  }
+
+  a {
     font-size: 30px;
     color: white;
   }
--- vue01/src/router/index.js
+++ vue01/src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 Vue.use(Router)
 import ComL1 from '../components/ComsL/ComL1'
 import First from '../components/first/FirstOne'
+import Zhang from  '../components/text_zhang/tem_zhang'
 export default new Router({
-  routes: [{path:'/zj1',component:ComL1},
-{path:'/zj2',component:First}
+  routes: [
+    {path: '/zj1', component: ComL1},
+    {path: '/zj2', component: First},
+    {path: '/zj3', component: Zhang},
   ]
 })
