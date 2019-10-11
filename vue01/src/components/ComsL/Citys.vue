5ff1519916ebcd3c155829e20d6999b397bf8d78
DESKTOP-5TBM399\lx <734906252@qq.com>
2019-10-11 08:26:54 +0800

    test

---
+++ vue01/src/components/ComsL/Citys.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Countrys"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue01/src/components/ComsL/J1L.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "J1L"
+    }
+</script>
+
+<style scoped>
+
+</style>
