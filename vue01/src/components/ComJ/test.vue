15f5f20c08a8b942a01a736043f6413a436e925c
DESKTOP-B6UF53T\sube <1070049256@qq.com>
2019-10-11 21:49:09 +0800

    J

---
+++ vue01/src/components/ComJ/test.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="SV">
+<div class="server">
+  <router-link :to="{}">返回</router-link>
+  <p>服务中心</p>
+</div>
+    <div>
+      <div class="tl">
+        <router-link :to="{}">
+          <img src="./kefu1.png" alt="">
+          <p>在线客服</p>
+        </router-link>
+      </div>
+      <div class="tl2">
+        <router-link :to="{}">
+          <img src="./kefu2.png" alt="">
+          <p>在线客服</p>
+        </router-link>
+      </div>
+    </div>
+      <p class="hot">热门问题</p>
+      <ul>
+        <li>{{}}</li>
+      </ul>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "test",
+      data(){
+          return{
+              msg:[],
+              des:[]
+          }
+      },
+      methods:{
+          ask(){
+            this.axios.get("https://elm.cangdu.org/v3/profile/explain").then((res)=>{
+              console.log(res.data);
+              this.des = res.data;
+                for(let a in res.data){
+                    this.msg.unshift(a);
+                }
+              this.msg.forEach(v=>{
+                console.log(this.des.v)
+              })
+
+              // console.log(this.des)
+            })
+          }
+      },
+      created(){
+          this.ask();
+      }
+    }
+</script>
+
+<style scoped>
+  .SV{
+    height: 100%;
+  }
+  a{
+    text-decoration: none;
+  }
+.server{
+  height: 3rem;
+  background-color: #3190e8;
+  text-align: center;
+  position: relative;
+}
+  .server>a{
+    font-size: 1rem;
+    position: absolute;
+    left: 0.2rem;
+    top:0.9rem;
+    color: white;
+    text-decoration: none;
+
+  }
+  .server>p{
+    font-size: 1rem;
+    color: white;
+    line-height: 3rem;
+  }
+.tl,.tl2{
+  height:6rem;
+  width: 50%;
+  /*background-color: darkgreen;*/
+  text-align: center;
+  padding: 0.5rem 0;
+}
+  .tl>a>img,.tl2>a>img{
+    height: 3.6rem;
+    width: 3.6rem;
+  }
+  .tl>a,.tl2>a{
+    color: black;
+    font-size: 0.9rem;
+  }
+.tl{
+  float: left;
+}
+  .tl2{
+    float: right;
+  }
+.hot{
+  line-height: 3rem;
+  padding-left: 1.5rem;
+}
+</style>
---
+++ vue01/src/components/ComJ/test重置密码.vue
@@ -0,0 +1,104 @@
+
+<template>
+  <div class="reF">
+    <div class="res">
+      <router-link :to="{}"><</router-link>
+      <p>重置密码</p>
+    </div>
+    <div class="reI">
+      <input type="text" placeholder="用户名"><br>
+      <input type="text" placeholder="旧密码"><br>
+      <input type="text" placeholder="新密码"><br>
+      <input type="text" placeholder="确认密码"><br>
+
+        <input type="text" placeholder="验证码">
+      <div class="img">
+        <img :src="arr" alt=""><br>
+      </div>
+      <div class="see">
+        <span>看不清</span>
+        <span style="color: blue" @click="yan">换一张</span>
+      </div>
+
+      <button class="btn">确认修改</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+    export default {
+        name: "test",
+      data(){
+        return{
+          arr:''
+        }
+      },
+      methods:{
+         yan(){
+           this.axios.post("https://elm.cangdu.org/v1/captchas").then((response) => {
+console.log(response.data.code);
+this.arr = response.data.code
+           })
+           }
+      },
+      created(){
+         this.yan()
+      }
+    }
+</script>
+
+<style scoped>
+  .reF{
+    height: 100%;
+    background-color: #f0f0f0;
+  }
+  .res{
+    height: 5rem;
+    background-color: #3190e8;
+    /*font-size: 1rem;*/
+    text-align: center;
+  }
+  p{
+    line-height: 5rem;
+    font-size: 1.5rem;
+    color: white;
+  }
+  a{
+    text-decoration: none;
+    position: absolute;
+    left: 1rem;
+    top: 1rem;
+    color: white;
+    font-size: 2.3rem;
+  }
+  input{
+    padding: 2.5rem;
+    margin: 0.05rem 0;
+    width: 100%;
+    border: none;
+    height: 2rem;
+  }
+  .btn{
+    width: 32rem;
+    color: white;
+    font-size: 2rem;
+    background-color: #4cd964;
+    margin-left: 3rem;
+    margin-top: 2rem;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+  }
+  .reI{
+    position: relative;
+  }
+  .img{
+    position: absolute;
+    top:21.5rem;
+    right: 5rem;
+  }
+  .see{
+    margin-left: 25rem;
+    margin-top: 1rem;
+  }
+</style>
